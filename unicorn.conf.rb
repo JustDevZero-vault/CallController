21dc34d4c686d08b7e0b20d4a2754467e41bf23e
Daniel Ripoll <meph.snake@gmail.com>
2015-05-31 10:26:43 +0200

    Create: unicorn.conf.rb

---
+++ unicorn.conf.rb
@@ -0,0 +1,16 @@
+$UNICORN = 1
+$ASYD_PID = Process.pid
+$ASYD_VERSION = 0.08
+$DBG = 0 #debug?
+
+FileUtils.mkdir("log") unless File.directory?("log")
+
+listen 3000
+worker_processes 1
+pid ".asyd.pid"
+stderr_path "log/callcontroller.log"
+stdout_path "log/callcontroller.log"
+
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) and
+  GC.copy_on_write_friendly = true
